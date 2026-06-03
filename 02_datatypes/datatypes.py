a0b74f7878f695d260ab3485407fe9549a3a6ebe
sameerkindia <sameerkhanaa5@gmail.com>
2026-06-03 20:53:50 +0530

    String and it's methods

--- 02_datatypes/datatypes.py
+++ 02_datatypes/datatypes.py
@@ -24,9 +24,29 @@ num_float = 1.5
 
 # If we perform a divison then result always come in float number
 
-print(f"10 divide by 2 with true divison {10 / 2}")
-print(f"10 divide by 2 with floor divison {10 // 2}")
+# print(f"10 divide by 2 with true divison {10 / 2}")
+# print(f"10 divide by 2 with floor divison {10 // 2}")
 
-print(f"abs method give us absolute value for ex. -10 with abs() is {abs(-10)}")
+# print(f"abs method give us absolute value for ex. -10 with abs() is {abs(-10)}")
 
-print(f"Round method {round(14.77777834, 3)}")
+# print(f"Round method {round(14.77777834, 3)}")
+
+
+# String
+
+name = "sameer"
+
+print(f"first letter of name {name[0]}")
+print(f"last letter of name {name[-1]}")
+print(f"total 'e' letter in name {name.count("e")}")
+print(f"name with uppercase {name.upper()}")
+print(f"name with titlecase {name.title()}")
+
+# print(f"name slicing {name[0:4]}")
+# print(f"name slicing {name[::2]}")
+# print(f"name slicing {name[0:4:2]}")
+
+
+print(f"replace word e with h {name.replace('e', 'h')}")
+print(f"finding index of substring mee {name.find('mee')}")
+print(f"spliting name into an array/list {name.split('e')}")
