41f53624e5c7520a0ea6ac473be2a7da573b731a
sameerkindia <sameerkhanaa5@gmail.com>
2026-06-03 20:37:36 +0530

    datatype Numbers

---
+++ 02_datatypes/datatypes.py
@@ -0,0 +1,32 @@
+print("hello this is data type module")
+
+# Numbers, Floats, Strings, Bool, Sets, Tople's are immutable
+
+# Numbers and Floats
+
+num_a = 5
+
+# print(f"this is num_a")
+# print(f"this is num_a's Id {id(num_a)}")
+
+num_a = 6
+
+# print(f"this is num_a after change")
+# print(f"this is num_a's Id after change {id(num_a)}")
+
+
+
+num_float = 1.5
+
+# print(f"Float number {num_float}")
+
+
+
+# If we perform a divison then result always come in float number
+
+print(f"10 divide by 2 with true divison {10 / 2}")
+print(f"10 divide by 2 with floor divison {10 // 2}")
+
+print(f"abs method give us absolute value for ex. -10 with abs() is {abs(-10)}")
+
+print(f"Round method {round(14.77777834, 3)}")
