b1908187ae0140ff7c8a09ada11fb92cd17065d9
Sameer Khan <sameerkhanaa5@gmail.com>
2026-08-27 11:18:53 +0530

    csv to json convertor

---
+++ 00_scripts/02_data_handling/07_CSV_to_JSON.py
@@ -0,0 +1,40 @@
+import os
+import json
+import csv
+
+INPUT_FILE = "contacts.csv"
+OUTPUT_FILE="contacts.json"
+
+
+def load_csv_data(filename):
+    if not os.path.exists(filename):
+        print("CSV file not found")
+        return []
+
+    with open(filename, "r", encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        data = list(reader)
+        return data
+
+def save_as_json(data, filename):
+    with open(filename, "w", encoding='utf-8') as f:
+         json.dump(data, f, indent=2)
+    print(f"✅ Converted {len(data)} records to {filename}")
+
+def preview_data(data, count=3):
+    for row in data[:count]:
+        print(json.dumps(row, indent=2))
+    print('...............')
+
+
+def main():
+    data = load_csv_data(INPUT_FILE)
+    if not data:
+        return
+
+    save_as_json(data, OUTPUT_FILE)
+    preview_data(data)
+
+
+if __name__ == "__main__":
+    main()
