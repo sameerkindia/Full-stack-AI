5f8dba64d4674763e59b661c328a9bbad214f174
Sameer Khan <sameerkhanaa5@gmail.com>
2026-09-16 11:41:23 +0530

    automation file organiser

---
+++ 00_scripts/04_automation/01_file_organiser.py
@@ -0,0 +1,83 @@
+# import os
+# import shutil
+
+
+# EXTENSION_MAP = {
+#     "PDFs": [".pdf"],
+#     "Images": [".png", ".jpeg", ".jpg"],
+#     "TextFiles": [".txt"]
+# }
+
+# def get_destination_folder(filename):
+#     ext = os.path.splitext(filename)[1].lower()
+#     for folder, extensions in EXTENSION_MAP:
+#         if ext in extensions:
+#             return folder
+#     return "Others"
+
+# def sort_files(folder_path):
+#     for file in os.listdir(folder_path):
+#        full_path = os.path.join(folder_path, file)
+
+#        if os.path.isfile(full_path):
+#            dest_folder = get_destination_folder(file)
+#            dest_path = os.path.join(folder_path, dest_folder)
+
+#            os.makedirs(dest_path, exist_ok=True)
+
+#         # Move the file
+#         shutil.move(full_path, os.path.join(dest_path, file))
+#         print(f"Moved : {file} -> {dest_folder}/")
+
+
+
+
+
+import os
+import shutil
+
+
+EXTENSION_MAP = {
+    "PDFs": [".pdf"],
+    "Images": [".png", ".jpeg", ".jpg"],
+    "TextFiles": [".txt"],
+    "pythonCode" : [".py"]
+}
+
+def get_destination_folder(filename):
+    ext = os.path.splitext(filename)[1].lower()
+
+    for folder, extensions in EXTENSION_MAP.items():
+        if ext in extensions:
+            return folder
+    return "Others"
+
+def sort_files(folder_path):
+    for file in os.listdir(folder_path):
+        full_path = os.path.join(folder_path, file)
+        
+        if "01_file_organiser.py" in full_path:
+            continue
+
+        if os.path.isfile(full_path):
+            dest_folder = get_destination_folder(file)
+            dest_path = os.path.join(folder_path, dest_folder)
+
+            os.makedirs(dest_path, exist_ok=True)
+
+            # Move the file
+            shutil.move(full_path, os.path.join(dest_path, file))
+            print(f"Moved : {file} -> {dest_folder}/")
+
+
+if __name__ == "__main__":
+    folder = input("Enter the folder path or leave blank: ").strip()
+    folder = folder or os.getcwd()
+    # print(f"This is curr folder {folder}")
+
+    if not os.path.isdir(folder):
+        print("Invalid directory")
+    else:
+        sort_files(folder)
+
+        print("✅ sorting completed")
